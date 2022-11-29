e2440ee91eb23808ad57dc18f1d0c6b53c9363dd
GabrielRodrigues07 <grodrigues256@gmail.com>
2022-11-29 10:39:53 -0300

    autenticando o usuario

--- src/main/java/br/com/alura/forum/configs/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/configs/security/SecurityConfigurations.java
@@ -1,6 +1,7 @@
 package br.com.alura.forum.configs.security;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,15 +9,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
+@AllArgsConstructor
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    private AutenticacaoService autenticacaoService;
 
     //configuracoes de autenticacao
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+        auth.userDetailsService(autenticacaoService)
+        .passwordEncoder(new BCryptPasswordEncoder());
     }
 
     //configuracoes de autenticacoes
