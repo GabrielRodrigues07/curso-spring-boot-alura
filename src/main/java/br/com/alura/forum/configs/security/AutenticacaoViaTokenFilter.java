ba251bfa2920c968306e96092f97e310e9dc1699
GabrielRodrigues07 <grodrigues256@gmail.com>
2022-11-30 10:37:52 -0300

    verificando se o token e valido

---
+++ src/main/java/br/com/alura/forum/configs/security/AutenticacaoViaTokenFilter.java
@@ -0,0 +1,33 @@
+package br.com.alura.forum.configs.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@AllArgsConstructor
+public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
+
+    private TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = recuperarToken(request);
+        boolean valido = tokenService.isTokenValido(token);
+        System.out.println(valido);
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+            return null;
+        }
+        return token.substring(7);
+    }
+}
--- src/main/java/br/com/alura/forum/configs/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/configs/security/SecurityConfigurations.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -20,6 +21,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
     private AutenticacaoService autenticacaoService;
+    private TokenService tokenService;
 
     @Override
     @Bean
@@ -48,6 +50,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/auth").permitAll()
         .anyRequest().authenticated()
         .and().csrf().disable()
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService), UsernamePasswordAuthenticationFilter.class);
     }
 }
